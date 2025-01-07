eb766a7e0bd3c368fce5f239ee0334caf6aca816
brixJordan <brixcarmelo@gmail.com>
2025-01-07 13:48:18 +0800

    trigger modal button for adding product

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -1,5 +1,7 @@
 @extends('adminlayoutnavigator.adminlayout')
 
 @section('content')
-    <h1>Product</h1>
+<button class="px-4 py-4 bg-red-500 text-white rounded hover:bg-red-600" id="openModal">Add Product</button>
+@include('adminModals.add_product_modal')
+    
 @endsection
--- resources/views/adminlayoutnavigator/adminlayout.blade.php
+++ resources/views/adminlayoutnavigator/adminlayout.blade.php
@@ -5,8 +5,8 @@
     <ul>
         <li><a href="{{route('admin.dashboard')}}" class="block py-2 px-4 hover:bg-blue-600">Home</a></li>
         <li><a href="{{route('admin.product')}}" class="block py-2 px-4 hover:bg-blue-600 ">Products</a></li>
-        <li><a href="{{route('admin.inventory')}}" class="block py-2 px-4 hover:bg-blue-600 ">Inventory</a></li>
         <li><a href="{{route('admin.sale')}}" class="block py-2 px-4 hover:bg-blue-600 ">Sales</a></li>
+        <li><a href="{{route('admin.inventory')}}" class="block py-2 px-4 hover:bg-blue-600 ">Inventory</a></li>
         <li><a href="{{route('admin.customer')}}" class="block py-2 px-4 hover:bg-blue-600 ">Customers</a></li>
     </ul>
 </div>
